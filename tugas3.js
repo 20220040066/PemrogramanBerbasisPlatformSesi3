a904231cb933c64b5610185a5118a224bc47f5a4
Silvaaa destiani <124055481+20220040066@users.noreply.github.com>
2023-10-20 18:48:43 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,24 @@
+let restaurantObject = { 
+    Name: "Cafe Enhypen", 
+    City: "Jakarta", 
+    Favorite_drink: "Mint Choco", 
+    Favorite_food: "Tteokbokki", 
+    isVegan: false 
+}; 
+ 
+let restaurantArray = [ 
+ { 
+    Name: "Cafe Enhypen", 
+    City: "Jakarta", 
+    Favorite_drink: "Mint Choco", 
+    Favorite_food: "Tteokbokki", 
+    isVegan: false 
+ }, 
+ { 
+    Name: "Treasure Cafe", 
+    City: "Jakarta", 
+    Favorite_drink: "Americano Coffe", 
+    Favorite_food: "Ramen", 
+    isVegan: true 
+ } 
+];
